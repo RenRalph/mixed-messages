ae4ecfb9d5858f2120378936ff10babad436d792
Ren Ralph <renralph222@gmail.com>
2021-12-21 22:07:04 +0000

    Work in progress

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-function RandomNum (num){
-    // Gets # from 0 -> num - 1
+//Codecademy Mixed Messages project
+
+function randomNum (num){
+    // Gets number from 0 to num-1
     return Math.floor(Math.random() * num)
 }
 
+// object of arrays to keep messages
+const collectiveAdvice = {
+    programLanguage: ['javascript', 'ruby', 'java', 'kotlin', 'php', 'go', 'c#'],
+    level: ['Newbie', 'Intermidiate', 'Senior', 'Master', 'Gran Master'],
+    tip: ['improve and improve', 'you should practice more', 'relax and learn']
+  }
+  
+  // array with full message
+  let nextStep = []
+  
+  // Iterate over the object considering the array length
+  for(let item in collectiveAdvice) {
+    let optionIdx = randomNum(collectiveAdvice[item].length)
+  
+    // use the object's properties to customize the message being added to collectiveAdvice 
+    switch(item) {
+      case 'programLanguage':
+        nextStep.push(`Your skill path right now is a "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      case 'level':
+        nextStep.push(`Your next target should be: "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      case 'tip':
+        nextStep.push(`You should: "${collectiveAdvice[item][optionIdx]}".`)
+        break
+      default:
+        nextStep.push('There is not enough info.')
+    }
+  }
+  
+  function formatWisdom(wisdom) {
+    const formatted = nextStep.join('\n')
+    console.log(formatted)
+  }
+  
+  formatWisdom(nextStep);
